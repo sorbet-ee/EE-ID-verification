c799964055207f8399c18dbb7223427460010099
Angelos Kapsimanis <angelos@sorbet.ee>
2025-08-05 13:17:32 +0300

    Empty gem

---
+++ lib/EE/ID/verification.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "verification/version"
+
+module EE
+  module ID
+    module Verification
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/EE/ID/verification/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module EE
+  module ID
+    module Verification
+      VERSION = "0.1.0"
+    end
+  end
+end
